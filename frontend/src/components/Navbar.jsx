54a01a86d28b487fc849faaedcad26587c9575f1
Nafi Bin Zafar <nafi.zafar179@gmail.com>
2025-01-28 03:24:46 +0600

    Navbar and Banner Complete

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,34 @@
 import { Link } from "react-router-dom";
-import { HiMiniBars3CenterLeft } from "react-icons/hi2";
+import { HiMiniBars3CenterLeft, HiOutlineShoppingCart } from "react-icons/hi2";
 import { IoSearchOutline } from "react-icons/io5";
+import { HiOutlineUser } from "react-icons/hi2";
+import { HiOutlineHeart } from "react-icons/hi2";
+import avatarImage from "../assets/avatar.png";
+import { useState } from "react";
+
+const navigation = [
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    name: "Orders",
+    href: "/orders",
+  },
+  {
+    name: "Cart Page",
+    href: "/cart",
+  },
+  {
+    name: "Checkout",
+    href: "/checkout",
+  },
+];
 
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const currentUser = true;
+
   return (
     <header className="max-w-screen-2xl mx-auto px-4 py-6">
       <nav className="flex justify-between items-center">
@@ -20,7 +46,55 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div>nav items</div>
+        <div className="relative flex items-center md:space-x-3 space-x-2">
+          <div>
+            {currentUser ? (
+              <>
+                <button
+                  onClick={() => setIsDropdownOpen((prevState) => !prevState)}
+                >
+                  <img
+                    src={avatarImage}
+                    alt="Avatar Image"
+                    className="size-7 rounded-full ring-2 ring-blue-500"
+                  />
+                </button>
+                {isDropdownOpen && (
+                  <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
+                    <ul className="py-2">
+                      {navigation.map((item) => (
+                        <li key={item.name}>
+                          <Link
+                            to={item.href}
+                            className="block px-4 py-2 hover:bg-gray-100"
+                            onClick={() => setIsDropdownOpen(false)}
+                          >
+                            {item.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+              </>
+            ) : (
+              <Link to={"/login"}>
+                <HiOutlineUser className="size-6" />
+              </Link>
+            )}
+          </div>
+          <button className="hidden sm:block">
+            <HiOutlineHeart className="size-6" />
+          </button>
+
+          <Link
+            to={"/cart"}
+            className="bg-primary p-1 sm:px-6 py-2 flex items-center rounded-sm"
+          >
+            <HiOutlineShoppingCart className="" />
+            <span className="text-sm font-semibold sm:ml-1">0</span>
+          </Link>
+        </div>
       </nav>
     </header>
   );
---
+++ frontend/src/pages/Home/Banner.jsx
@@ -0,0 +1,29 @@
+import bannerImg from "../../assets/banner.png";
+
+const Banner = () => {
+  return (
+    <div className="flex flex-col md:flex-row-reverse py-16 justify-between items-center gap-12">
+      <div className="md:w-1/2 w-full flex items-center md:justify-end">
+        <img src={bannerImg} alt="" />
+      </div>
+
+      <div className="md:w-1/2 w-full">
+        <h1 className="md:text-5xl text-2xl font-medium mb-7">
+          New Releases This Week
+        </h1>
+        <p className="mb-10">
+          It&apos;s time to update your reading list with some of the latest and
+          greatest releases in the literary world. From heart-pumping thrillers
+          to captivating memoirs, this week&apos;s new releases offer something
+          for everyone
+        </p>
+
+        <button className="bg-yellow-500 text-black px-6 py-2 rounded-md shadow hover:bg-yellow-600 transition duration-300 ease-in-out transform hover:scale-105">
+          Subscribe
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
