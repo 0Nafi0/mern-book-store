8453405cdd0c70c1004fdc0fc75c4f5c3d09a2c5
Nafi Bin Zafar <nafi.zafar179@gmail.com>
2025-01-28 15:09:04 +0600

    Added TopSeller section that have BookCard and Banner Component

---
+++ frontend/src/pages/Books/BookCard.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/prop-types */
+
+import { FiShoppingCart } from "react-icons/fi";
+import { getImageURL } from "../../utils/getImageURL";
+
+import { Link } from "react-router-dom";
+
+const BookCard = ({ book }) => {
+  return (
+    <div className=" rounded-lg transition-shadow duration-300">
+      <div className="flex flex-col sm:flex-row sm:items-center sm:h-72  sm:justify-center gap-4">
+        <div className="sm:h-72 sm:flex-shrink-0 border rounded-md">
+          <Link to={`/books/${book._id}`}>
+            <img
+              src={`${getImageURL(book?.coverImage)}`}
+              alt=""
+              className="w-full bg-cover p-2 rounded-md cursor-pointer hover:scale-105 transition-all duration-200"
+            />
+          </Link>
+        </div>
+
+        <div>
+          <Link to={`/books/${book._id}`}>
+            <h3 className="text-xl font-semibold hover:text-blue-600 mb-3">
+              {book?.title}
+            </h3>
+          </Link>
+          <p className="text-gray-600 mb-5">
+            {book?.description.length > 80
+              ? `${book.description.slice(0, 80)}...`
+              : book?.description}
+          </p>
+          <p className="font-medium mb-5">
+            ${book?.newPrice}{" "}
+            <span className="line-through font-normal ml-2">
+              $ {book?.oldPrice}
+            </span>
+          </p>
+          <button className="bg-yellow-500 text-black px-6 py-2 rounded-md shadow flex items-center gap-2 hover:bg-yellow-600 transition duration-300 ease-in-out transform hover:scale-105">
+            <FiShoppingCart className="" />
+            <span>Add to Cart</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookCard;
