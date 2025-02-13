1c5485c1f1686cc28602fac37c6cec2334f8066a
nishat <nishat.tahsin090@gmail.com>
2025-02-13 23:41:39 +0600

    Create Blog Schema & Model,Book Routes & Controllers

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const app = express();
+const cors=require("cors");
 const mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
-require("dotenv").config();
+//const port = process.env.PORT || 5000;
+//require("dotenv").config();
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+
+// middleware
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}))
 
 app.use("/", (req, res) => {
   res.send("Book Store server is running");
 });
-
+//routes
+const bookRoutes =require('./src/books/book.route.js')
+app.use("/api/books",bookRoutes)
 async function main() {
   await mongoose.connect(process.env.DB_URI);
 }
@@ -16,6 +29,6 @@ main()
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
---
+++ backend/src/books/book.route.js
@@ -0,0 +1,25 @@
+const express =require('express')
+const Book = require('./book.model');
+const { postABook, getAllBooks, getSingleBook, UpdateBook, deleteABook } = require('./book.controller');
+const router = express.Router();
+
+// frontend => backend server => controller => book schema  => database => send to server => back to the frontend
+//post= when submit smtg fronted to db
+//get= when get smtg back from db
+//put/patch=when edit or update smtg
+//dlt
+//post a book
+router.post("/create-book",postABook)
+
+// get all books
+router.get("/", getAllBooks);
+
+// single book endpoint
+router.get("/:id", getSingleBook);
+
+// update a book endpoint
+router.put("/edit/:id",  UpdateBook);
+router.delete("/:id",  deleteABook)
+
+module.exports=router;
+
